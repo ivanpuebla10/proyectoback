ff07969ec4ed7c516a85fc4dc21a453c652a96d4
ivanpuebla10 <ivanpuebla94@gmail.com>
2022-01-20 17:18:33 +0100

    Tablas sin relaciones terminadas

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ const PORT = 3000
 app.use(express.json())
 
 app.use('/productos', require('./routes/productos'));
+app.use('/categorias', require('./routes/categorias'));
+app.use('/usuarios', require('./routes/usuarios'));
+app.use('/pedidos', require('./routes/pedidos'));
+
 
 app.listen(PORT, () => console.log('servidor levantado en el puerto' + PORT))
